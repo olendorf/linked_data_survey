a0a330ae638e7bb7b55e5c3467d9f9a07d201b95
Adam Chandler <alc28@cornell.edu>
2018-01-04 09:14:43 -0500

    adams file updated

--- survey_analysis_ac.R
+++ survey_analysis_ac.R
@@ -26,47 +26,63 @@ survey = read_csv('ld_results_final__for_rob_recoded_trimmed.csv', col_names = T
 # Mush all th etext columns together to increase power
 survey$combined_words <- paste(survey$linked_data_description, survey$linked_data_benefits, survey$additional_thoughts)
 
+
 # Tokenize the words
-tokenized_survey <- survey %>% unnest_tokens(word, combined_words)
+tokenized_survey_word_counts <- survey %>% 
+  unnest_tokens(word, combined_words) %>%
+  count(response_id, word, sort=TRUE) %>%
+  ungroup()
+
+total_words <- tokenized_survey_word_counts %>%
+  group_by(response_id) %>%
+  summarize(total = sum(n))
+
+tokenized_survey_word_counts <- left_join(tokenized_survey_word_counts, total_words)
+
+
+freq_by_rank <- tokenized_survey_word_counts %>%
+  group_by(response_id) %>%
+  mutate(rank = row_number(), 'term frequency' = n/total)
+
+
+tokenized_tf_idf <- tokenized_survey_word_counts %>%
+  bind_tf_idf(term_col = word, document_col = response_id, n_col = n)
+
+tokenized_tf_idf %>%
+  arrange(desc(tf_idf)) %>%
+  group_by(response_id) %>%
+  top_n(2)
+
 
 # Remove stop words and join
 data("stop_words")
 tokenized_survey <- tokenized_survey %>% anti_join(stop_words)
 
+# raw count after tokenizing by word
+tokenized_word_counts <- tokenized_survey %>% 
+  count(word, sort = TRUE)
 
-# NRC changes the structre a bit since each word in the word column can get 
-# multiple NRC sentiments. Making a new dataframe makes it a little easier
-# for the other sentiment measure.
-tokenized_survey_nrc <- tokenized_survey %>% inner_join(get_sentiments("nrc"))
-
-# The rest can just go in to the main data frame.
-tokenized_survey <- tokenized_survey %>% inner_join(get_sentiments("bing"))
-tokenized_survey$bing_sentiment <- tokenized_survey$sentiment
-
-tokenized_survey$sentiment <- NULL
-
-tokenized_survey <- tokenized_survey %>% inner_join(get_sentiments("afinn"))
-tokenized_survey$afinn_sentiment <- tokenized_survey$score
-
-tokenized_survey$score <- NULL
+  
 
 
-bing_word_counts <- tokenized_survey %>% 
-  count(word, bing_sentiment, sort = TRUE) %>%
-  ungroup()
+# review bigrams
 
-bing_word_counts %>%
-  group_by(bing_sentiment) %>%
-  top_n(7) %>%
-  ungroup() %>%
-  mutate(word = reorder(word, n)) %>%
-  ggplot(aes(word, n, fill = bing_sentiment)) +
-  geom_col(show.legend = FALSE) +
-  facet_wrap(~bing_sentiment, scales = "free_y") +
-  labs(y = "Contribution to sentiment",
-       x = NULL) +
-  coord_flip()
+survey$participant_number = 1:nrow(survey)
 
+combined_bigrams <- survey %>%
+  unnest_tokens(bigram, combined_words, token = "ngrams", n = 2) %>% 
+  select(participant_number, bigram) %>% 
+  arrange(participant_number)
+
+bigram_counts <- combined_bigrams %>% 
+  count(bigram, sort = TRUE)
+
+bigram_tf_idf <- combined_bigrams %>%
+  count(participant_number, bigram) %>%
+  bind_tf_idf(bigram, participant_number, n) %>%
+  arrange(desc(tf_idf))
+
+bigram_tf_idf
 
 
 
