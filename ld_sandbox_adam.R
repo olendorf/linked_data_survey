10efc041062942a1173ee294706fbdadeb71f77f
Adam Chandler <alc28@cornell.edu>
2018-01-11 16:43:04 -0500

    adams sandbox

--- ld_sandbox_adam.R
+++ ld_sandbox_adam.R
@@ -2,9 +2,57 @@
 
 library(tidyverse)
 library(readxl)
+library(tidytext)
+library(wordcloud)
+library(ggplot2)
+
+data(stop_words)
+
 
 ld_results_rob_adam <- read_excel("ld_results_rob_adam.xlsx")
 
 df <- ld_results_rob_adam %>%
   mutate(benefits_total = benefits_score + adam_benefits_score)
+
+
+
+# word cloud
+text_df <- df %>%
+  unnest_tokens(word, linked_data_benefits) %>% 
+  anti_join(stop_words)
+text_df %>% count(word) %>% with(wordcloud(word, n, max.words = 60))
+dev.copy(jpeg,filename="plots/benefits_wordcloud.jpg");
+dev.off ();
+
+# word cloud of high benefits
+text_df_high_benefits <- df %>%
+  filter(benefits_total == 2) %>%
+  unnest_tokens(word, linked_data_benefits) %>% 
+  anti_join(stop_words)
+
+text_df_high_benefits %>% count(word) %>% with(wordcloud(word, n, max.words = 60))
+dev.copy(jpeg,filename="plots/high_benefits_wordcloud.jpg");
+dev.off ();
+
+
+# term frequency
+
+benefits_words <- df %>%
+  filter(benefits_total == 2) %>%
+  unnest_tokens(word, linked_data_benefits) %>%
+  count(response_id, word, sort = TRUE) %>%
+  ungroup()
+
+total_words <- benefits_words %>% 
+  group_by(response_id) %>% 
+  summarize(total = sum(n))
+
+benefits_words <- left_join(benefits_words, total_words)
+
+freq_by_rank <- benefits_words %>% 
+  group_by(response_id) %>% 
+  mutate(rank = row_number(), 
+         `term frequency` = n/total)
+
+freq_by_rank
 
